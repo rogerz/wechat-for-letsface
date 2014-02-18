c899f713d7b287ea927e2af9dfaf6580c2672b69
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-02-18 22:58:50 +0800

    Simple demo for menu events

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-module.exports = function () {
-  return 'Hello, world';
-};
+var connect = require('connect');
+var wechat = require('wechat');
+var config = require('./wx_config');
+
+var app = connect();
+
+function oops(msg, req, res, next) {
+  res.reply({
+    "type": "text",
+    "content": "oops"
+  });
+}
+
+app.use(connect.logger('dev'))
+.use(connect.query())
+.use(wechat(config.token)
+
+.text(oops)
+.image(oops)
+.voice(oops)
+.video(oops)
+.location(oops)
+.link(oops)
+.event(function (msg, req, res, next) {
+  if (msg.Event === 'CLICK') {
+    var key = msg.EventKey;
+    var json;
+    try {
+      json = require('./assets/messages/' + key.toLowerCase());
+    } catch(e) {
+      oops(msg, req, res, next);
+      return;
+    }
+    res.reply(json);
+  }
+})
+.middlewarify()
+
+);
+
+app.listen(3000, function () {
+  console.log("wechat bot listening on 3000");
+});
